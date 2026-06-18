a012849946658396ff12d3b27295ca6c99a08c59
almamun2b <almamun2b@gmail.com>
2026-06-19 03:48:24 +0600

    animation added

--- app/components/icons/Play.tsx
+++ app/components/icons/Play.tsx
@@ -31,8 +31,8 @@ const PlayIcon = ({ className }: LogoProps) => {
         className="opacity-50 group-hover:opacity-65"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M39 26.0359C41.6667 27.5755 41.6667 31.4245 39 32.9641L26.25 40.3253C23.5833 41.8649 20.25 39.9404 20.25 36.8612L20.25 22.1388C20.25 19.0596 23.5833 17.1351 26.25 18.6747L39 26.0359Z"
         fill="#1E3E85"
         className="origin-center group-hover:scale-105"
